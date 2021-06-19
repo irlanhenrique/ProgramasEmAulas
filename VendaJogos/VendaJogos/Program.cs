be1bdd26c2f406b4ff47bceef32282a52cf1a703
Irlan <irlansh@hotmail.com>
2021-06-19 00:05:19 -0300

    Feito campo nome e classificação do Jogo

---
+++ VendaJogos/VendaJogos/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using VendaJogos.ViewModels;
+
+namespace VendaJogos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gta = new JogoViewModel();
+
+            Console.WriteLine("Qual é o nome do jogo");
+            var nomeJogo = Console.ReadLine();
+            gta.Nome = nomeJogo;
+
+            Console.WriteLine("Informe a classificação do jogo");
+            
+            var classificacaoPreenchida = false;
+
+            while (!classificacaoPreenchida)
+            {
+                var classificacaoQueVemDaTelaEmTexto = Console.ReadLine();
+
+                if (byte.TryParse(classificacaoQueVemDaTelaEmTexto, out byte classicacaoQueVemDaTelaEmByte))
+                {
+                    gta.ClassificacaoCensura = classicacaoQueVemDaTelaEmByte;
+                    classificacaoPreenchida = true;
+                }
+                else
+                {
+                    Console.WriteLine("Valor invalido, por favor, informe apenas nmeros");
+                }
+            }
+
+
+            gta.DataLancamento = new DateTime(2022, 1, 30);
+            gta.Descricao = "Jogo do GTA";
+            gta.Garantia = "Pode trocar em até 3 meses da data da compra";
+            gta.Generos = new string[]
+            {
+                "Ação",
+                "Tiro",
+                "Aventura"
+            };
+            gta.IdiomasAudios = new string[] 
+            {
+                "Português",
+                "Inglês"
+            };
+            gta.IdiomasLegendas = new string[]
+            {
+                "Português",
+                "Inglês"
+
+            };
+            gta.MarcaProdutoraEstudio = "Rockstar";
+            gta.PermiteOnline = true;
+            gta.Plataforma = "àlystation";
+            gta.QuantidadeJogadores = 1;
+        }
+    }
+}
---
+++ VendaJogos/VendaJogos/ViewModels/JogoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendaJogos.ViewModels
+{
+    public class JogoViewModel
+    {
+        public string Nome { get; set; }
+        public string Plataforma { get; set; }
+        public byte QuantidadeJogadores { get; set; }
+        public bool PermiteOnline { get; set; }
+        public byte ClassificacaoCensura { get; set; }
+        public string MarcaProdutoraEstudio { get; set; }
+        public string[] IdiomasAudios { get; set; }
+        public string[] IdiomasLegendas { get; set; }
+        public string Garantia { get; set; }
+        public string[] Generos { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
